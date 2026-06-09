1f30c9a4a03c5a5fbe2e72f21754ecb3076eeff2
vova <hohiop09@gmail.com>
2026-06-09 21:23:24 +0200

    chore:added a keys

---
+++ app.config.ts
@@ -0,0 +1,34 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'finly',
+  slug: 'finly',
+  scheme: 'finly',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router'],
+  extra: {
+    revenueCatApiKey: process.env.REVENUECAT_API_KEY,
+  },
+});
