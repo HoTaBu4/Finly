3320452fbea7df31e8f2287ea9eb69189091608e
vova <hohiop09@gmail.com>
2026-09-02 20:47:28 +0200

    add

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ module.exports = ({ config }) => ({
   name: 'Finzelo',
   slug: 'trackfin',
   scheme: 'finzelo',
-  version: '1.2.7',
+  version: '1.2.8',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
